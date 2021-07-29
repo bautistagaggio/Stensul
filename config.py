7e170c145bca1b734e9ed4db6e10b3d222f691b7
Bautista_Gaggiotti <68549231+bautistagaggio@users.noreply.github.com>
2021-07-29 18:13:35 -0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-class TestData:
-
-    FileDirectory = "sdfdsf"
-    Text = "fdsfds"
-    Text300 = "String with more than 300 characters"
-    CHROME_EXECUTABLE_PATH = ""
-    FIREFOX_EXECUTABLE_PATH = ""
-    Item1 = ""
-    text2 = "Creators: Matt Duffer, Ross Duffer"
